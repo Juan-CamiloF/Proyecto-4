c7e7fc8f7f26c95077c5b6f3c3bbd337cb4e95de
Juan-CamiloF <jucamilogac@outlook.com>
2021-01-14 18:52:15 -0500

    Calculadora

--- assets/scripts/scrip.js
+++ assets/scripts/scrip.js
@@ -1,7 +1,11 @@
-let resul = document.getElementById('resul')
+
+let resul = document.getElementById('resul');
+
+let alertaN1 = document.getElementById('alertaN1');
+let alertaN2 = document.getElementById('alertaN2');
 function operacion(op){
-    let n1=parseInt(document.getElementById('n1').value)
-    let n2=parseInt(document.getElementById('n2').value)
+    let n1=parseInt(document.getElementById('n1').value);
+    let n2=parseInt(document.getElementById('n2').value);
     switch(op){
         case "suma":
             resul.innerText=n1+n2;
@@ -13,7 +17,11 @@ function operacion(op){
             resul.innerText=n1*n2;
             break;
         case "divi":
-            resul.innerText=n1/n2;
+            if(n2==0){
+                resul.innerText ="Error"
+            }else{
+                resul.innerText=n1/n2;
+            }
             break;
         default:
            
